b6283a395407645e7d4367065872e92b5ebfd42a
gabesone <sena6.gabriel@gmail.com>
2024-09-20 19:59:36 -0300

    config target path

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
       "/api": {
         target: "https://movies-api-gabesone.vercel.app",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, "/api"),
+        rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
   },
