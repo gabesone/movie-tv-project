c1b0afb888ff580052f6920d82388ec96627e40e
gabesone <sena6.gabriel@gmail.com>
2024-10-03 22:17:18 -0300

    increasy z-index to fix issue when display on small screens

--- frontend/src/components/TopNav.jsx
+++ frontend/src/components/TopNav.jsx
@@ -5,7 +5,7 @@ function TopNav({ children }) {
   const navigate = useNavigate();
 
   return (
-    <nav className="sticky bottom-0 left-0 right-0 top-0 z-10 flex h-11 items-center justify-center bg-black text-gray-100 xl:hidden">
+    <nav className="sticky bottom-0 left-0 right-0 top-0 z-20 flex h-11 items-center justify-center bg-black text-gray-100 xl:hidden">
       <button
         className="absolute bottom-0 left-0 right-0 top-0 flex h-full w-11 items-center justify-center text-xl focus:bg-purple-600"
         onClick={() => navigate(-1)}
