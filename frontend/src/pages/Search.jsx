8ff6d9550b5db64c29d5e2064d6ffd72be54b968
gabesone <sena6.gabriel@gmail.com>
2024-10-28 10:44:09 -0300

    Change z-index from Search component

--- frontend/src/pages/Search.jsx
+++ frontend/src/pages/Search.jsx
@@ -2,7 +2,7 @@ function Search({ isOpen }) {
   return (
     <>
       {isOpen && (
-        <div className="sticky top-0 z-20 h-20 w-full bg-slate-500">
+        <div className="sticky top-0 z-30 h-20 w-full bg-slate-500">
           {/* <input type="text" /> */}
         </div>
       )}
