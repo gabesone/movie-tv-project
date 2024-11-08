711d838bd523f30918880f2d9fb581800ce0d5bc
gabesone <sena6.gabriel@gmail.com>
2024-11-08 19:20:44 -0300

    Add css atributes to the External component

--- frontend/src/components/External.jsx
+++ frontend/src/components/External.jsx
@@ -1,7 +1,11 @@
-function External({ children }) {
+function External({ link, name, children }) {
   return (
-    <ul className="text-xl hover:text-purple-600 sm:text-2xl">
-      <li>{children}</li>
+    <ul className="text-xl transition-colors duration-300 hover:text-purple-600 sm:text-2xl">
+      <li>
+        <a href={link} alt={`Page of ${name}`} target="_blank">
+          {children}
+        </a>
+      </li>
     </ul>
   );
 }
