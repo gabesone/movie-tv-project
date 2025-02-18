18dee80bccfc03da3bcde3f99ce3bf30dead452f
gabesone <sena6.gabriel@gmail.com>
2025-02-18 13:22:43 -0300

    Added keyboard navigation support to tab movie view options

--- frontend/src/components/SelectedItem.jsx
+++ frontend/src/components/SelectedItem.jsx
@@ -4,7 +4,7 @@ function SelectedItem({ children, selected, setSelected }) {
       onClick={() => setSelected(children)}
       className={`${selected === children && "bg-transparent text-gray-100 transition-all duration-300 xl:underline xl:underline-offset-[1rem]"} w-full cursor-pointer bg-gray-800 py-3 text-center transition-all duration-300 hover:text-gray-100 xl:w-auto xl:bg-transparent`}
     >
-      {children}
+      <button className="uppercase">{children}</button>
     </li>
   );
 }
